923092f9d3c709a55a047a557795baee230d4b48
Bever116 <134636795+Bever116@users.noreply.github.com>
2025-10-28 14:08:15 +0300

    fix: import discord module in game module

--- apps/api/src/game/game.module.ts
+++ apps/api/src/game/game.module.ts
@@ -3,8 +3,10 @@ import { GameService } from './game.service';
 import { GameController } from './game.controller';
 import { GameGateway } from './game.gateway';
 import { InviteController } from './invite.controller';
+import { DiscordModule } from '../discord/discord.module';
 
 @Module({
+  imports: [DiscordModule],
   providers: [GameService, GameGateway],
   controllers: [GameController, InviteController],
   exports: [GameService]
