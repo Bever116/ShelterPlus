3963b1f24e2dae14c84c11ad4dc571ffc81f41dd
Bever116 <134636795+Bever116@users.noreply.github.com>
2025-10-28 16:39:51 +0300

    Configure NestJS CORS for localhost credentials

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: corsConfig });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(corsConfig);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
