3110786410b95069b81b6a25d6a0c349f5ef692c
Bever116 <134636795+Bever116@users.noreply.github.com>
2025-10-28 04:19:24 +0300

    Add comprehensive runbook instructions

---
+++ apps/api/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '*.js'],
+};
